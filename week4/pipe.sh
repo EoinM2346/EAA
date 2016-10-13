cd0c856e038073a8ea068f73046c46ce63818b18
John Burns <jzburns@gmail.com>
2016-10-13 10:34:49 +0100

    Piping

---
+++ week4/pipe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#ps -ef : prints a snapshot of every process in the system
+
+#grep mysql : searches for the text mysql from the list of processes on the system
+
+#This is a new a new command:
+#searches all the running processes looking for the mysql process
+
+ps -ef | grep mqsql 
+
+
